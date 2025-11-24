0116ed3c51df27abcfb595f559484bd0a607aaec
Vedanand <125856769+VedanandRoy@users.noreply.github.com>
2025-11-25 04:59:12 +0530

    Add files via upload

---
+++ _blogs.php
@@ -0,0 +1,36 @@
+
+<!-- Blogs -->
+<section id="blogs">
+    <div class="container py-4">
+        <h4 class="font-rubik font-size-20">Latest Blogs</h4>
+        <hr>
+
+        <div class="owl-carousel owl-theme">
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Upcoming Mobiles</h5>
+                    <img src="./assets/blog/blog1.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">Samsung India has introduced a new feature, ‘Bixby Cricket’, to enhance its voice assistant, Bixby. This update provides cricket match scores, World Cup points table updates, and schedules for upcoming matches for users in India.</p>
+                    <a href="#" class="color-second text-left">Go somewhere</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Upcoming Mobiles</h5>
+                    <img src="./assets/blog/blog2.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">Microsoft has announced its active efforts to comply with the Digital Markets Act (DMA) in the European Economic Area (EEA).</p>
+                    <a href="#" class="color-second text-left">Go somewhere</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Upcoming Mobiles</h5>
+                    <img src="./assets/blog/blog3.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">OnePlus has started rolling out a new update (v13.2.0.116) for the latest OnePlus Open, the company’s first foldable phone weeks after the sale. The minor update brings eSIM support, as the company had confirmed.</p>
+                    <a href="#" class="color-second text-left">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- !Blogs -->
